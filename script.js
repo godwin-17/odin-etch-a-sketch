2e5b042027f757cdae087f5c7ac86ab2cafbd566
Godwin <agblezeg@gmail.com>
2022-09-04 12:33:29 +0200

    Change grid display from inline-block to grid

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  container.appendChild(div);
+function makeGrid(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+
+  for(let i = 0; i < (rows * cols); i++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  }
 }
 
-const divs = document.querySelectorAll(".container div");
-console.log(divs);
-
-divs.forEach(div => {
-  div.addEventListener("mouseover", ()=> {
-    div.style.backgroundColor = "blue";
-  });
-});
+makeGrid(16, 16);
