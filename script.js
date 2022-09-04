af5742bb05bee8b89910ba8ae1000b8ec5cb751f
Godwin <agblezeg@gmail.com>
2022-09-04 10:42:43 +0200

    Add hover effect

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 256; i++) {
   let div = document.createElement("div");
   container.appendChild(div);
 }
+
+const divs = document.querySelectorAll(".container div");
+console.log(divs);
+
+divs.forEach(div => {
+  div.addEventListener("mouseover", ()=> {
+    div.style.backgroundColor = "red";
+  });
+});
